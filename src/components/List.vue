44cfa5a484bf3bf21fd51244eb38aed663c99627
Alex <kaka_alex@inbox.ru>
2025-10-16 22:00:43 +0300

    Добавлена библиотека axios и написан метод get в методе onMounted

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,7 +1,16 @@
 <script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const list = ref([])
+onMounted(async() => {
+    const response = await axios.get('api/Usluga')
+    list.value = response.data
+})
 </script>
 
 <template>
+
 </template>
 
 <style scoped>
