453400b8bed05cad5eeda5f4d65cfcd4aae550e8
Alex <kaka_alex@inbox.ru>
2025-10-16 23:03:26 +0300

    Сделан переход между 2 первыми страницами

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <script setup>
+import { ref } from 'vue';
+import AddPage from './components/AddPage.vue';
 import List from './components/List.vue';
 
+const showAddPage = ref(false)
 function ShowAddPage()
 {
-
+  showAddPage.value = !showAddPage.value
 }
 </script>
 
 <template>
-  <div v-if="!ShowAddPage">
+  <div v-if="!showAddPage">
   <List/>
-  <button class="button-add">Перейти на страницу добавления</button>
+  <button @click="ShowAddPage" class="button-add">Перейти на страницу добавления</button>
   </div>
   <div v-else>
-  <button class="button-add">Назад</button>
+  <AddPage/>
+  <button @click="ShowAddPage" class="button-back">Назад</button>
   </div>
 </template>
 
@@ -24,6 +28,15 @@ function ShowAddPage()
     margin: 10px;
 }
 .button-add:hover {
+    font-size: 17px;
+    background-color: darkgreen;
+}
+.button-back {
+    background-color: green;
+    border-radius: 10px;
+    margin: 10px;
+}
+.button-back:hover {
     font-size: 17px;
     background-color: darkgreen;
 }
--- src/components/AddPage.vue
+++ src/components/AddPage.vue
@@ -6,7 +6,9 @@ const list = ref([])
 </script>
 
 <template>
-
+<div>
+    <h2>Страница добавления услуги</h2>
+  </div>
 </template>
 
 <style scoped>
