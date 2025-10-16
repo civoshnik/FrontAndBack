d48585fce288e5697ecc480afc34755e2dab5954
Alex <kaka_alex@inbox.ru>
2025-10-16 22:24:57 +0300

    Создана разметка для первой страницы и кнопка для перехода на вторую

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,17 @@ import List from './components/List.vue';
 
 <template>
   <List/>
+  <button class="button-add">Перейти на страницу добавления</button>
 </template>
 
 <style scoped>
+.button-add {
+    background-color: green;
+    border-radius: 10px;
+    margin: 10px;
+}
+.button-add:hover {
+    font-size: 17px;
+    background-color: darkgreen;
+}
 </style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -4,14 +4,43 @@ import axios from 'axios';
 
 const list = ref([])
 onMounted(async() => {
-    const response = await axios.get('api/Usluga')
+    const response = await axios.get('/api/Usluga')
     list.value = response.data
 })
 </script>
 
 <template>
-
+    <div class="text-center">
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Айди услуги</th>
+                        <th>Статус</th>
+                        <th>Наименование</th>
+                        <th>Тип услуги</th>
+                        <th>Цена</th>
+                        <th>Дата создания</th>
+                        <th>Дата редактирования</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="usluga in list" :key="usluga.id">
+                        <td>{{ usluga.id }}</td>
+                        <td>{{ usluga.status }}</td>
+                        <td>{{ usluga.name }}</td>
+                        <td>{{ usluga.type }}</td>
+                        <td>{{ usluga.price }}</td>
+                        <td>{{ usluga.createdAt }}</td>
+                        <td>{{ usluga.modifiedAt }}</td>
+                    </tr>
+                </tbody>
+            </table>
+            
+        </div>
+    </div>
 </template>
 
 <style scoped>
+
 </style>
