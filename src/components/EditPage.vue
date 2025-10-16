9bff9ff84dc791da91dde4269a9aada9cb29e64f
Alex <kaka_alex@inbox.ru>
2025-10-17 01:55:58 +0300

    Прописана логика для обновления всего заказа

--- src/components/EditPage.vue
+++ src/components/EditPage.vue
@@ -8,44 +8,31 @@ const name = ref("")
 const type = ref("")
 const price = ref("")
 
-async function Post() {
+
+async function Update() {
     const fulldata = {
         name: name.value,
         type: type.value,
         price: parseFloat(price.value)
     }
-    const response = await axios.post('api/Usluga', fulldata)
-
-    name = ""
-    type = ""
-    price = ""
-
+    const response = await axios.put('/api/put', fulldata)
+   name = ""
+   type = ""
+   price = "" 
 }
 </script>
 
 
 <template>
 <div class="text-center">
-    <h1>Страница добавления услуги</h1>
+    <h1>Страница редактирования услуги</h1>
     <p><input type="text" v-model="name" placeholder="Введите наименование услуги"/></p>
     <p><input type="text" v-model="type" placeholder="Введите тип услуги"/></p>
     <p><input type="text" v-model="price" placeholder="Введите цену"/></p>
-    <button @click="Post">Добавить!</button>
+    <button @click="Update">Добавить!</button>
   </div>
 </template>
 
 <style scoped>
-input {
-    width: 300px;
-    padding: 10px;
-    border-radius: 8px;
-    border: 1px solid gray;
-    font-size: 16px;
-}
-input:focus {
-    background-color: black;
-}
-input:hover {
-    padding:11px;
-}
+
 </style>
