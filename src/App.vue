c8fe4c7712c76647f903cbac55944147edd95ee6
Alex <kaka_alex@inbox.ru>
2025-10-16 22:52:03 +0300

    Добавлена вторая страница вью

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script setup>
 import List from './components/List.vue';
+
+function ShowAddPage()
+{
+
+}
 </script>
 
 <template>
+  <div v-if="!ShowAddPage">
   <List/>
   <button class="button-add">Перейти на страницу добавления</button>
+  </div>
+  <div v-else>
+  <button class="button-add">Назад</button>
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/AddPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const list = ref([])
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
