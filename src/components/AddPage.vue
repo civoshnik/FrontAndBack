08cac062f2be08b70df2c225a8fcc7e8be8ad6f5
Alex <kaka_alex@inbox.ru>
2025-10-19 00:52:40 +0300

    НАстроена валидация полей

--- src/components/AddPage.vue
+++ src/components/AddPage.vue
@@ -1,9 +1,24 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import { useRouter } from 'vue-router'
+import * as yup from 'yup'
 import axios from 'axios';
 
 const router = useRouter()
+
+const errors = ref({
+    name: "",
+    type: "",
+    price: ""
+
+})
+
+const validation = yup.object({
+    name: yup.string().required('Наименование должно быть обязательным'),
+    type: yup.string().required('Тип должен быть обязательным'),
+    price: yup.number().typeError('Цена должна быть число').positive('Цена должна быть больше 0')
+})
+
 
 const list = ref([])
 
@@ -17,11 +32,25 @@ async function Post() {
         type: type.value,
         price: parseFloat(price.value)
     }
-    const response = await axios.post('api/Usluga', fulldata)
-
+    try {
+        await validation.validate(fulldata, { abortEarly: false})
+        errors.value = {}
+        const response = await axios.post('api/Usluga', fulldata)
     name.value = ""
     type.value = ""
     price.value = ""
+    router.push('/')
+    }
+    catch (err){
+        if(err.inner) {
+            errors.value = {}
+            err.inner.forEach(e => {
+                errors.value[e.path] = e.message
+            });
+        }
+
+    }
+    
 
 }
 </script>
@@ -30,8 +59,11 @@ async function Post() {
 <template>
 <div class="text-center">
     <h1>Страница добавления услуги</h1>
+    <p style="color: red;">{{ errors.name }}</p>
     <p><input type="text" v-model="name" placeholder="Введите наименование услуги"/></p>
+    <p style="color: red;">{{ errors.type }}</p>
     <p><input type="text" v-model="type" placeholder="Введите тип услуги"/></p>
+    <p style="color: red;">{{ errors.price }}</p>
     <p><input type="text" v-model="price" placeholder="Введите цену"/></p>
     <button @click="Post">Добавить!</button>
   </div>
--- src/components/EditPage.vue
+++ src/components/EditPage.vue
@@ -1,12 +1,27 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router'
+import * as yup from 'yup'
 import { useRouter } from 'vue-router';
 import axios from 'axios';
 
 const route = useRoute()
 const router = useRouter()
 const id = route.params.id
+
+const validation = yup.object({
+    name: yup.string().required('Наименование должно быть обязательным'),
+    type: yup.string().required('Тип должен быть обязательным'),
+    price: yup.number().required('Цена должна быть обязательная').positive('Цена должна быть больше 0')
+})
+
+const errors = ref({
+    name: "",
+    type: "",
+    price: ""
+})
+
+
 
 const list = ref([])
 
@@ -22,11 +37,23 @@ async function Update() {
         type: type.value,
         price: parseFloat(price.value)
     }
+    try {
+        await validation.validate(fulldata, { abortEarly : false })
+        errors.value = {}
     const response = await axios.put(`/api/Usluga/${id}`, fulldata)
     name.value = ""
     type.value = ""
     price.value = ""
     router.push('/')
+    }
+    catch (err) {
+        if(err.inner) {
+            errors.value = {}
+            err.inner.forEach(e => {
+                errors.value[e.path] = e.message
+            });
+        }
+    }
 }
 onMounted(async () => {
     const response = await axios.get(`/api/Usluga/${id}`)
@@ -41,8 +68,11 @@ onMounted(async () => {
 <template>
 <div class="text-center">
     <h1>Страница редактирования услуги</h1>
+    <p style="color: red;">{{ errors.name }}</p>
     <p><input type="text" v-model="name" placeholder="Введите наименование услуги"/></p>
+    <p style="color: red;">{{ errors.type }}</p>
     <p><input type="text" v-model="type" placeholder="Введите тип услуги"/></p>
+    <p style="color: red;">{{ errors.price }}</p>
     <p><input type="text" v-model="price" placeholder="Введите цену"/></p>
     <button @click="Update">Отредактиовать!</button>
   </div>
